c0b0069c054c1272bca3b43127bdfb03861ef4d8
Hrayr <hmkrtchy@uoguelph.ca>
2020-07-30 19:43:06 -0400

    first commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+# Create your views here.
+
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
+    context_object_name = 'YEEHAW'
