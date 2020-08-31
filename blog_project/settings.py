964755ff74235c6da74db92cdde6b481bedd2596
Hrayr <hmkrtchy@uoguelph.ca>
2020-08-31 19:40:15 -0400

    'Heroku'

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6c0u!!y&9%iit##3@%6)@2)nu5bol_(#oy!$++p5vo!69)$x32'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
